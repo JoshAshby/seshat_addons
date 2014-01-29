fc67c40b0053f9dd9f4dbb3faf143e7136c62b94
JoshAshby <joshuaashby@joshashby.com>
2014-01-28 18:30:57 -0700

    not sure if this will work, might have to rename the seshat addons folder

--- seshat_addons/__init__.py
+++ seshat_addons/__init__.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 __version__ = '0.1.0'
 VERSION = tuple(map(int, __version__.split('.')))
+
+import .seshat
+import .view
+
+__all__ = [
+    "seshat.func_mods",
+    "seshat.mixed_object",
+    "seshat.obj_mods",
+    "seshat.request_item",
+    "view.template"
+    ]
