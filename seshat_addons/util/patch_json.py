f8eb6d39f93482b2055a8ac70998d4509471f712
JoshAshby <joshuaashby@joshashby.com>
2014-03-03 17:39:46 -0700

    custom support for parsing things with __json__

---
+++ seshat_addons/util/patch_json.py
@@ -0,0 +1,14 @@
+"""Module that monkey-patches json module when it's imported so
+JSONEncoder.default() automatically checks for a special "__json__()"
+method and uses it to encode the object if found.
+
+Stolen from the wonderful answer by martineau here:
+    http://stackoverflow.com/a/18561055
+"""
+from json import JSONEncoder
+
+def _default(self, obj):
+    return getattr(obj.__class__, "__json__", _default.default)(obj)
+
+_default.default = JSONEncoder().default # save unmodified default
+JSONEncoder.default = _default # replacement
